6594fbe827b59f21657753b3032b287395b0a611
Guiii <gnailpo895@gmail.com>
2024-11-02 19:10:11 +0000

    HHAHAHA FUNÇÃO PARA DEPOSITAR DINHEIRO

---
+++ project.js
@@ -0,0 +1,15 @@
+// 1. Depositar algum dinheiro na carteira
+// 2. Determinar numero de linhas para apostar
+// 3. Coletar o dinheiro
+// 4. Girar a slot machine
+// 5. Verificar se o user ganhou 
+// 6. Dar o dinheiro ao utilizador 
+// 7. JOGAR DE NOVO
+
+//Função para depositar dinheiro na carteira
+function deposit (){
+
+
+}
+
+deposit()
