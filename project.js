b9f42e7f5ab5ded002dcb79c3055894c4d7d9195
Guiii <gnailpo895@gmail.com>
2024-11-04 23:06:38 +0000

    ????????????????????

--- project.js
+++ project.js
@@ -1,24 +1,10 @@
-// 1. Depositar algum dinheiro na carteira (Feito);
-// 2. Determinar numero de linhas para apostar (Feito);
-// 3. Coletar o dinheiro (Por fazer);
-// 4. Girar a slot machine (Por fazer);
-// 5. Verificar se o user ganhou (Por fazer);
-// 6. Dar o dinheiro ao utilizador (Por fazer);
-// 7. JOGAR DE NOVO (GAMBLINGGGGGGGGG) (Por fazer);
-
-//Função para depositar dinheiro na carteira
-//function deposit (){
-
-//}
-
 const prompt = require("prompt-sync")();
 
-//ter sempre os imports em cima do codigo
+// ter sempre os imports em cima do codigo
 const ROWS = 3;
 const COLS = 3;
 
-//ter sempre os simbolos em cima do codigo
-//nao e preciso usar "" para as keys
+// ter sempre os simbolos em cima do codigo
 const SYMBOLS_COUNT = {
   A: 2,
   B: 3,
@@ -26,7 +12,7 @@ const SYMBOLS_COUNT = {
   D: 8,
 };
 
-//Valores dos símbolos
+// Valores dos símbolos
 const SYMBOL_VALUES = {
   A: 5,
   B: 4,
@@ -34,6 +20,7 @@ const SYMBOL_VALUES = {
   D: 2,
 };
 
+// Função para depositar dinheiro na carteira
 const deposit = () => {
   while (true) {
     const depositAmount = prompt("Deposita o montante: ");
@@ -47,7 +34,7 @@ const deposit = () => {
   }
 };
 
-//Função para determinar o número de linhas para apostar
+// Função para determinar o número de linhas para apostar
 const getNumberLinesToBet = () => {
   while (true) {
     const Lines = prompt(
@@ -63,6 +50,7 @@ const getNumberLinesToBet = () => {
   }
 };
 
+// Função para definir o valor da aposta por linha
 const getBet = (balance, Lines) => {
   while (true) {
     const bet = prompt("Deposite o numero total da aposta por linha: ");
@@ -76,7 +64,7 @@ const getBet = (balance, Lines) => {
   }
 };
 
-//todos os símbolos disponíveis e metelos em um array e meter random
+// Função para girar a slot machine, criar rolos e selecionar símbolos
 const spin = () => {
   const symbols = []; // para fazer um array basta fazer []
   for (const [symbol, count] of Object.entries(SYMBOLS_COUNT)) {
@@ -84,18 +72,52 @@ const spin = () => {
       symbols.push(symbol);
     }
   }
-  const reels = [[], [], []]; // para fazer um array
+
+  const reels = [[], [], []]; // Criando array para cada coluna
   for (let i = 0; i < COLS; i++) {
-    const reelsSymbols = [...symbols]; //copia os simbolos para outro array
+    const reelsSymbols = [...symbols]; // copia os simbolos para outro array
     for (let j = 0; j < ROWS; j++) {
-      //The Math.floor() method rounds a number DOWN to the nearest integer.
-      //Math.random ira gerar um numero random entre 0 e 1
-      const randomIndex = Math.floor(Math.random() * reelSymbols.length);
-      //ira buscar a variavel criada la em cima para meter random
-      const selectedSymbol = reelSymbols[randomIndex];
+      // The Math.floor() method rounds a number DOWN to the nearest integer.
+      // Math.random ira gerar um numero random entre 0 e 1
+      const randomIndex = Math.floor(Math.random() * reelsSymbols.length);
+      // irá buscar a variavel criada la em cima para meter random
+      const selectedSymbol = reelsSymbols[randomIndex];
+      reels[i].push(selectedSymbol);
+      reelsSymbols.splice(randomIndex, 1); // remove o simbolo que foi usado
     }
   }
+  return reels;
 };
+
+// Função para transpor os rolos para exibir as linhas jogadas
+const transpose = (reels) => {
+  const rows = [];
+
+  for (let i = 0; i < ROWS; i++) {
+    rows.push([]);
+    for (let j = 0; j < COLS; j++) {
+      rows[i].push(reels[j][i]);
+    }
+  }
+  return rows;
+};
+const printRows = (rows) => {
+  for (const row of rows) {
+    let rowString = "A";
+    for (const [i, symbol] of row.entries()) {
+      rowString += symbol;
+      if (i != row.length - 1) {
+        rowString += " |";
+      }
+    }
+    console.log(rowString);
+  }
+};
+// Execução do jogo
 let balance = deposit();
 const numberOfLines = getNumberLinesToBet();
 const bet = getBet(balance, numberOfLines);
+const reels = spin();
+const rows = transpose(reels);
+console.log(reels);
+console.log(rows);
